3230b556d2c420580a66169eb20cc70a56a93ded
Wendell Rodrigues Amaral <wendellrodrigues97@hotmail.com>
2022-02-02 20:04:15 -0300

    styling links page

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -1,12 +1,15 @@
 import './links.css'
 import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
 
 export default function Links(){
     return(
       <div className="links-container">
 
         <div className="links-header">
-          <FiArrowLeft size={38} color="#FFF" />
+          <Link to="/">
+          <FiArrowLeft size={38} color="#FFF" />          
+          </Link>
           <h1>Meus links</h1>
         </div>
 
@@ -15,7 +18,17 @@ export default function Links(){
             <FiLink   size={18} color="#FFF"/>
             https://google.com
           </button>
-          <button>
+          <button className="link-delete">
+            <FiTrash size={24} color="#FF5454" />
+          </button>
+        </div>
+
+        <div className="links-item">
+          <button className="link">
+            <FiLink   size={18} color="#FFF"/>
+            https://google.com
+          </button>
+          <button className="link-delete">
             <FiTrash size={24} color="#FF5454" />
           </button>
         </div>
