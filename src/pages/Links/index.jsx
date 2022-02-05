14e5852f75e488fdbb005f57472561fdba5284ef
Wendell Rodrigues Amaral <wendellrodrigues97@hotmail.com>
2022-02-04 22:14:00 -0300

    localStorage configured

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -4,13 +4,37 @@ import './links.css'
 import {FiArrowLeft, FiLink, FiTrash} from 'react-icons/fi'
 import {Link} from 'react-router-dom'
 import {getLinkSave} from '../../Services/storeLinks'
+import LinkItem from '../../components/LinkItem'
 
 
 export default function Links(){
 
+  const  [myLinks, setMyLinks] = useState([]);
+  const [data, setData] = useState({});
+  const [showModal, setShowModal] = useState(false)
+
   useEffect (()=>{
-    
-  })
+    async function getLinks(){
+      const result = await getLinkSave('@encurtaLink')
+
+      if(result.length === 0){
+        //lista vazia
+        console.log("lista vazia");
+      }
+      
+      setMyLinks(result);
+    }  
+
+    getLinks();
+
+  },
+  [])
+
+
+  function handleOpenLink(link) {
+   setData(link);
+   setShowModal(true)    
+  }
 
 
     return(
@@ -23,16 +47,25 @@ export default function Links(){
           <h1>Meus links</h1>
         </div>
 
-        <div className="links-item">
-          <button className="link">
+      {myLinks.map(link => (
+        <div key={link.id} className="links-item">
+          <button className="link" onClick={()=> handleOpenLink(link)}>
             <FiLink   size={18} color="#FFF"/>
-            https://google.com
+            {link.long_url}
           </button>
+
           <button className="link-delete">
             <FiTrash size={24} color="#FF5454" />
           </button>
-        </div>
+        </div>))
+      }
 
+      {showModal && (
+        <LinkItem
+          closeModal={ ()=> setShowModal(false)}
+          content={data}
+        />
+      )}
 
       </div>
     )
